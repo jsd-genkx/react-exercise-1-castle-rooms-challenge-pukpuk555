47dab648b3ed953a981cb0ef8f4684d97e8af8bc
pukpuk555 <jester2540@gmail.com>
2024-05-30 13:08:23 +0700

    complete without css

--- src/components/02_Tower.jsx
+++ src/components/02_Tower.jsx
@@ -1,5 +1,13 @@
-const Tower = () => {
-  return <div>Tower</div>;
+import SecretRoom from "./09_SecretRoom";
+
+const Tower = ({ message1, onReply }) => {
+  return (
+    <div>
+      <h1>Tower</h1>
+      <p>Message for JSD7: {message1}</p>
+      <SecretRoom message1={message1} onReply={onReply} />
+    </div>
+  );
 };
 
 export default Tower;
--- src/components/09_SecretRoom.jsx
+++ src/components/09_SecretRoom.jsx
@@ -1,5 +1,23 @@
-const SecretRoom = () => {
-  return <div>SecretRoom</div>;
+import { useState } from "react";
+
+const SecretRoom = ({ message1, onReply }) => {
+  const [reply, setReply] = useState("");
+
+  const handleSendReply = () => {
+    onReply(reply);
+  };
+  return (
+    <div>
+      <h1>SecretRoom</h1>
+      <p>Message for JSD7: {message1}</p>
+      <textarea
+        value={reply}
+        onChange={(e) => setReply(e.target.value)}
+        placeholder="Type your reply here"
+      />
+      <button onClick={handleSendReply}>Send Reply</button>
+    </div>
+  );
 };
 
 export default SecretRoom;
